4dab94ffbc2021d86d615da088e6864f879a8438
Gustavo Sanchez <gustavosr98@gmail.com>
2019-06-03 10:37:10 -0400

    Arreglado problema con Bootstrap React

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './routers/AppRouter';
 import 'normalize.css/normalize.css'; // LIMPIA LOS STYLES POR DEFECTO DEL NAVEGADOR
+import 'bootstrap/scss/bootstrap.scss';
 import './styles/styles.scss'; // SETEA LOS ESTILOS DE LA CARPETA styles/
 
 // SE INICIA LA APP

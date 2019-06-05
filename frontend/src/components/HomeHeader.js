d13320b394ed72af46f4b6cf8eb20817273ac485
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-04 21:02:54 -0400

    Estilos de segunda cabecera

--- frontend/src/components/HomeHeader.js
+++ frontend/src/components/HomeHeader.js
@@ -12,10 +12,17 @@ export class HomeHeader extends React.Component {
 
 
     render = () => (
+        <div className="HeaderContainer">
         <div className="HeaderHome">
             <img src="images/logo.png" className="HeaderHomeLogo"/>
             <div className="HeaderTitle">MinerUCAB</div>
             <div className="LoginButton">Iniciar sesión <i className="fa fa-sign-in-alt"></i></div>
+        </div>
+        <div className="SubHeaderHome">
+            <div className="HeaderSubTitle y">Lorem</div>
+            <div className="HeaderSubTitle g">Ipsum</div>
+            <div className="HeaderSubTitle b">Sit Amet</div>
+        </div>
         </div>
     )
 }

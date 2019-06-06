6972140c42b7735796836629abfdf820f0588583
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-06 16:31:03 -0400

    Desplazamiento animado con rueda

--- frontend/src/components/SubHeader.js
+++ frontend/src/components/SubHeader.js
@@ -1,30 +1,42 @@
 import React from 'react';
 
-
+window.addEventListener("wheel",function (event)
+{
+    if(event.deltaY>50)
+    {
+        SubHeader.move(Math.min(3,SubHeader.number+1));
+    }
+    else if(event.deltaY<-50)
+    {
+        SubHeader.move(Math.max(0,SubHeader.number-1));
+    }
+});
 
 export class SubHeader extends React.Component {
 
     constructor(props){
-        super(props)
+        super(props);
+        SubHeader.number=0;
         this.state = {
             showDashboard : false
         }
     }
 
-    move(number)
+    static move(number)
     {
         window.scrollTo({
             'behavior': 'smooth',
             'left': 0,
             'top': window.outerHeight*number*0.95
         });
+        SubHeader.number=number;
 
     }
     render = () => (
         <div className="SubHeaderHome">
-            <div onClick={()=>this.move(0)} className="HeaderSubTitle y">Historia</div>
-            <div onClick={()=>this.move(1)} className="HeaderSubTitle b">Nosotros</div>
-            <div onClick={()=>this.move(2)} className="HeaderSubTitle g">Contacto</div>
+            <div onClick={()=> SubHeader.move(0)} className="HeaderSubTitle y">Historia</div>
+            <div onClick={()=> SubHeader.move(1)} className="HeaderSubTitle b">Nosotros</div>
+            <div onClick={()=> SubHeader.move(2)} className="HeaderSubTitle g">Contacto</div>
         </div>
     )
 }

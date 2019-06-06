2f50d91f331c1b529e72e3d6eb9beacfcbe1275b
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-06 17:21:48 -0400

    Creación del panel principal

---
+++ frontend/src/pages/MainPanel.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export class MainPanel extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    login  = () => {
+
+    }
+
+    render = () => (
+        <div>
+           Estoy en el panel
+        </div>
+    )
+}

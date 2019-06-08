278144833c5464307e3318ba5233344e12cd9375
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-08 19:33:18 -0400

    Fondo oscuro dashboard

--- frontend/src/components/MenuDashBoard.js
+++ frontend/src/components/MenuDashBoard.js
@@ -14,24 +14,29 @@ export class MenuDashBoard extends React.Component {
     static clickMenu()
     {
         let element = document.getElementById("DashboardMenu");
+        let back = document.getElementById("DarkBackground");
         if(element.classList.contains("Up"))
         {
             element.classList.remove("Up");
             element.classList.add("Down");
+            back.style.visibility="visible";
         }
         else
         {
             element.classList.remove("Down");
             element.classList.add("Up");
+            back.style.visibility="hidden";
         }
 
     }
     render = () => (
-        <div className="TestPlayGround">
+        <div>
+            <div className="DarkBackground" id="DarkBackground">
+            </div>
             <HeaderLogin hideMenuButton={this.props.main}/>
             <div id="DashboardMenu" className="DashBoardMenu Up">
-                <DashBoardSimple main={false}/>
-            </div>
+                <DashBoardSimple main={false}/></div>
+
         </div>
     )
 }

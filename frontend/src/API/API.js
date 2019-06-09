e50bfe21e2578b9af384996538038364e951a62a
Gustavo Sanchez <gustavosr98@gmail.com>
2019-06-09 18:29:19 -0400

    CRUD Mineral - Solo falta funcionalidad de eliminar

--- frontend/src/API/API.js
+++ frontend/src/API/API.js
@@ -10,7 +10,13 @@ const API = {
                         esRadioactivo: false,
                         nacionalizado: null,
                         yacimientos : ["La Guaira", "Alemania", "Trinidad"],
-                        compuestos : ["Hierro", "Bauxita"],
+                        compuestos : [{
+                                id : 32,
+                                nombre :"Hierro"
+                            },{
+                                id : 37,
+                                nombre :"Bauxita"
+                        }],
                         descripcion: "is simply dummy text of the printing and typesettingly dummy text of the printing and typesettingly dummy text of the printing and typesettingly dummy text of the printing and typesettingly dummy text of the printing and typesettingly dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                     },
                     {
@@ -18,7 +24,7 @@ const API = {
                         nombre: "Plata",
                         esMetal: true,
                         esRadioactivo: true,
-                        nacionalizado: "15/05/1998",
+                        nacionalizado: "1998-05-15",
                         yacimientos : ["Delta Amacuro", "Tucupita", "Barquisimeto"],
                         descripcion: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia"
                     }

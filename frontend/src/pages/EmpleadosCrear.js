1ffda60a2cd8ef76cbb29a3ca1f3d464ed03b428
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-06-13 15:23:04 -0400

    Empiezo crear empleado

--- frontend/src/pages/EmpleadosCrear.js
+++ frontend/src/pages/EmpleadosCrear.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {MenuDashBoard} from "../components/MenuDashBoard";
 import {Dropdown} from "../components/Dropdown";
 import {InputText} from "../components/InputText";
+import {SectionTitle} from "../components/Header/SectionTitle";
 
 export class EmpleadosCrear extends React.Component {
     constructor(props){
@@ -19,16 +20,24 @@ export class EmpleadosCrear extends React.Component {
     render = () => (
         <div>
             <MenuDashBoard/>
+            <SectionTitle title="Crear Empleado"/>
             <div className="RowContainer">
                 <div className="WideContainer">
-                    <Dropdown placeholder="Cargo" id="Cargo..."
-                              options={["Prueba0","Prueba1","Prueba2","Prueba3","Prueba4","Prueba5"]}/>
+                    <div className="FormContainer">
+                        <InputText id="CrearEmpleadoNombre" label="Nombre"/>
+                        <InputText id="CrearEmpleadoApellido" label="Apellido"/>
+                        <InputText id="CrearEmpleadoCédula" label="Número de cédula"/>
+                        <Dropdown id="CrearEmpleadoCargo" placeholder="Cargo..." options={["Opción 1","Opción 2","Opción 3","Opción 4","Opción 5"]}/>
+
+                    </div>
                 </div>
                 <div className="WideContainer">
-                    <br/><br/>
-                    <InputText id="CrearNombreEmpleado" label="Nombre"/>
+
                 </div>
                 <div className="WideContainer">
+                    <div className="FormContainer">
+                        <img src="resources/img/Empleado.png" alt="" width="80%" style={{margin: "0 auto"}}/>
+                    </div>
                 </div>
             </div>
         </div>

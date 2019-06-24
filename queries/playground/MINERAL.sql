d9e24aa69c969fbc5b6adc38c67e38d498e208e7
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-06-24 11:53:26 -0400

    Algunos cambios para probar mineral

--- queries/playground/MINERAL.sql
+++ queries/playground/MINERAL.sql
@@ -2,7 +2,7 @@
 	m_id_mineral 	SERIAL,
 	m_nombre	VARCHAR(30) NOT NULL,
 	/* !!! OJO !!! REVISAR SUPERCLASE QUE LA PROF QUERIA PARA METALICOS */
-	m_metalico	VARCHAR(5),
+	m_tipo	VARCHAR(15),
 	m_radioactivo	VARCHAR(5),
 	m_fecha_nacionalizacion	DATE,
 	m_descripcion	VARCHAR(255),

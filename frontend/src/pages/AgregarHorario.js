92a02902b34737157a0f1c2afc30e08f61557bda
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-06-29 20:52:20 -0400

    Detalle al seleccionar bloques lejanos

--- frontend/src/pages/AgregarHorario.js
+++ frontend/src/pages/AgregarHorario.js
@@ -5,6 +5,8 @@ import {Modal, Button} from 'react-bootstrap';
 import {Redirect} from 'react-router-dom';
 import {MenuDashBoard} from "../components/MenuDashBoard";
 import {Scheduler} from "../components/Scheduler";
+import {InputText} from "../components/InputText";
+import {GuardarCancelar} from "../components/GuardarCancelar";
 
 
 export class AgregarHorario extends React.Component {
@@ -12,14 +14,40 @@ export class AgregarHorario extends React.Component {
         super(props)
 
         this.state  = {
+            h_nombre: "",
             goBack : false
         }
     }
+    handleChange = (target) => {
+        target=target.target||target;
+        this.setState({
+            [target.name]: target.value
+        })
+    }
 
+    handleGua = (target) => {
+        target=target.target||target;
+        this.setState({
+            [target.name]: target.value
+        })
+    }
     render = () => (
         <div>
             <MenuDashBoard title="Agregar horario"/>
+            <div className="WideContainer" style={{maxWidth: "500px", margin:"3% 0"}}>
+                <InputText
+                    id="CrearHorarioNombre"
+                    label="Nombre"
+                    name="h_nombre"
+                    onChange={this.handleChange}/>
+            </div>
             <Scheduler editable={true}/>
+            <GuardarCancelar
+                position="center"
+                storeData={this.handleGuardar}
+                success={this.goEmpleado}
+                decline={this.goEmpleado}
+            />
                 {this.state.goBack && <Redirect to="/dashboard" /> }
 
         </div>

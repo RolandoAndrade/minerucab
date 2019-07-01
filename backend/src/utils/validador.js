0316e01996291909a9f05b8cc51827c2d74355d2
Yeisson Venencia <yeissonvenencia@gmail.com>
2019-06-30 22:52:24 -0400

    modificar e insertar con validaciones

--- backend/src/utils/validador.js
+++ backend/src/utils/validador.js
@@ -1,32 +1,40 @@
 const validadorYacimientoConfiguracion = {
     validar (y)  {
-        if (!y) return false
-        if (y.y_capacidad_explotacion <= 0 ) return false
-        if (!y.etapas) return false
-        if (y.etapas.length === 0) return false
+        let flag = ""
+        if (!y) flag = "Configuracion vacia"
+        if (y.y_capacidad_explotacion <= 0 ) flag = "y_capacidad_explotacion < 0"
 
-        for(let i = 0; i < y.etapas.length; i++){
-            let e = y.etapas.find((e) => e.e_orden === i+1)
-            if (!e) return false
-        }
-
-        y.etapas.forEach((e,j) => {
-            if (!e.fases) return false
-            if (e.fases.length === 0) return false
-
-            for(let i = 0; i < e.fases.length; i++){
-                let e = e.fases.find((e) => e.e_orden === i+1)
-                if (!e) return false
-            }
-
-            e.fases.forEach((f,k) => {
-                if (f.f_duracion <= 0 ) return false
-                if (!f.cargos) return false
-                if (f.cargos.length === 0) return false
+        if (y.requisitos && y.requisitos.length > 0)
+            y.requisitos.forEach((r) => {
+                if (r.m_cantidad <= 0) flag = "cantidad de requisito negativa"
             })
-        })
-
-        return true
+                        
+        if (!y.etapas) flag = "configuracion sin etapas"
+        else if (y.etapas.length === 0) flag = "configuracion sin etapas"
+        else{
+            for(let i = 0; i < y.etapas.length; i++){
+                let e = y.etapas.find((et) => et.e_orden === i+1)
+                if (!e) flag = `etapa de orden ${i+1} no presente`
+            }
+    
+            y.etapas.forEach((e) => {
+                if (!e.fases) flag = "etapa sin fases"
+                else if (e.fases.length === 0) flag = "etapa sin fases"
+                else {
+                    for(let i = 0; i < e.fases.length; i++){
+                        let f = e.fases.find((fa) => fa.f_orden === i+1)
+                        if (!f) flag = `etapa de orden ${i+1} no presente`
+                    }
+        
+                    e.fases.forEach((f) => {
+                        if (f.f_duracion <= 0 ) flag = "fase de duracion negativa"
+                        if (!f.cargos) flag = "fase sin cargos"
+                        if (f.cargos.length === 0) flag = "fase sin cargos"
+                    })
+                }                
+            })
+        }
+        return flag
     }
 }
 

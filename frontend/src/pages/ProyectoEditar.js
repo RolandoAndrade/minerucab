53c6bd6206e47834d38182220dd41d38ebf2c3e1
Gustavo Sanchez <gustavosr98@gmail.com>
2019-07-16 15:49:53 -0400

    final

--- frontend/src/pages/ProyectoEditar.js
+++ frontend/src/pages/ProyectoEditar.js
@@ -1092,7 +1092,7 @@ export class ProyectoEditar extends React.Component {
 
         const estado_id = configuracion_yacimiento.estado_id
 
-        const permisoModificar = tengoPermiso( 27 )
+        const permisoModificar = true
     
         return (
         <div>   
@@ -1106,13 +1106,13 @@ export class ProyectoEditar extends React.Component {
                             <div className="confYacimientoIzq">
                                 { !!estado_id && estado_id === 3 ?
                                     <div style={{margin : "5px auto"}}>
-                                        <Button disbaled={!permisoModificar} variant="warning" className="mc-boton" onClick={this.solicitarRecursos}>
+                                        <Button  variant="warning" className="mc-boton" onClick={this.solicitarRecursos}>
                                             Solicitar Recursos
                                         </Button>
                                     </div> 
                                 : !!estado_id && estado_id === 15 ?
                                     <div style={{margin : "5px auto"}}>
-                                        <Button disbaled={!permisoModificar} variant="warning" className="mc-boton" onClick={this.activarProyecto}>
+                                        <Button  variant="warning" className="mc-boton" onClick={this.activarProyecto}>
                                             Activar Proyecto
                                         </Button>
                                     </div>
@@ -1286,7 +1286,7 @@ export class ProyectoEditar extends React.Component {
                         }
                     </div>
                 </div>
-                <div>{ !!permisoModificar && 
+                <div>{ !!tengoPermiso(27) && 
                     <GuardarCancelar
                         position="center"
                         storeData={ estado_id === 3 ? this.guardarBD : this.goProyecto}
